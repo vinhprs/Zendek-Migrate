86a93404df451bcc55b3c598c0ce682ef421560f
Vinh Nguyễn <vinhnguyen19052002@gmail.com>
2023-05-29 11:17:27 +0700

    config module

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,16 +5,18 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './users/schema/users.entity';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     TypeOrmModule.forRoot({
       type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: '19052002',
-      database: 'auth-demo',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      username: process.env.DB_USERNAME,
+      password:  process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
       entities: [User],
       synchronize: true,
     }),
